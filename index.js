9068c65f41ef142918c06f5263f824e412cb3656
Zack Stout <zackstout@Zacks-MacBook-Pro.local>
2021-03-30 09:08:45 -0500

    Add d3 display of number of lines per speaker

--- index.js
+++ index.js
@@ -16,18 +16,40 @@ const plays = [
     // {
     //     title: "coriolanus",
     //     actScenes: [10, 3, 3, 7, 6]
+    // },
+    // {
+    //     title: "1henryiv",
+    //     actScenes: [3, 4, 2, 4, 5]
+    // },
+    // {
+    //     title: "hamlet",
+    //     actScenes: [5, 2, 4, 7, 2]
+    // },
+    // {
+    //     title: "macbeth",
+    //     actScenes: [7, 4, 6, 3, 8]
     // }
     {
-        title: "1henryiv",
-        actScenes: [3, 4, 2, 4, 5]
+        title: "measure",
+        actScenes: [4, 4, 2, 6, 1]
     },
     {
-        title: "hamlet",
-        actScenes: [5, 2, 4, 7, 2]
+        title: "midsummer",
+        actScenes: [2, 2, 2, 2, 1]
     },
     {
-        title: "macbeth",
-        actScenes: [7, 4, 6, 3, 8]
+        title: "much_ado",
+        actScenes: [3, 3, 5, 2, 4]
+    }, {
+        title: "tempest",
+        actScenes: [2, 2, 3, 1, 1]
+    }, {
+        title: "troilus_cressida",
+        actScenes: [3, 3, 3, 5, 10]
+    },
+    {
+        title: "twelfth_night",
+        actScenes: [5, 5, 4, 3, 1]
     }
 ];
 
@@ -63,6 +85,8 @@ async function getText(play) {
 
         const $ = cheerio.load(r.data);
 
+        let speaker = "";
+
         $("a").each((i, el) => {
             // NOTE this misses the intro text to each scene,
             // and presumably any stage directions along the way
@@ -75,12 +99,13 @@ async function getText(play) {
             if (name.includes("speech")) {
                 text = $(el).find("b").text();
                 type = "speech";
+                speaker = text;
             } else {
                 //  text = el.children[0]?.data;
                 text = $(el).text();
             }
             // console.log("el", name, text);
-            scene.lines.push({ type, value: text });
+            scene.lines.push({ type, value: text, speaker });
         });
 
         // plays.find(p => p.title === play.title).scenes.push(scene);
@@ -99,7 +124,9 @@ async function run() {
             const file = JSON.stringify(play);
             const playTitle = play[0].play;
             console.log("writing....", playTitle);
-            fs.writeFileSync(`output/${playTitle}.json`, file);
+            // const speakers = getLinesBySpeaker(play);
+            // console.log("speakers", speakers);
+            fs.writeFileSync(`public/assets/${playTitle}.json`, file);
         });
 
     } catch (e) {
@@ -107,5 +134,48 @@ async function run() {
     }
 }
 
+
+// D3 RIDGE PLOT!
+// https://bl.ocks.org/armollica/3b5f83836c1de5cca7b1d35409a013e3
+
+function getLinesBySpeaker(scenes) {
+    const speakers = {};
+
+    scenes.forEach(scene => {
+        scene.lines.forEach(line => {
+            if (line.type === "text") {
+                if (!speakers.hasOwnProperty(line.speaker)) {
+                    speakers[line.speaker] = 0;
+                }
+                speakers[line.speaker]++;
+            }
+        });
+    });
+
+    return speakers;
+}
+
 run();
 // console.log("scene 1", scenes[0].title, scenes[0].lines);
+
+
+
+/*
+
+Basic idea: (rap)Genius, but better
+
+So....we'll need a database..
+
+- Select a chunk oof text; keypress to do a specificc actiono
+- Add words to tags, like "sad" or "duty-based", so ccan highlight all in same color
+- Filter by speaker; show alll their lines, spaced out by scene
+(does this mean we should store as chunks?)
+- add charactere relational maps somehow
+- graphs of number of linse per speaker per scene (3d??? with each succeessive speaker line receding into bg?)
+- oor d3?
+
+
+
+
+
+*/
